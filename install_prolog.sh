04e05039d9ccf62c5724003670e1e437fbf5d16a
Adham Zahran <adham.zahran@hotmail.com>
2015-12-10 21:52:50 +0200

    initial commit

---
+++ install_prolog.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#TODO: check if user is root and exit the script if it's not root
+
+#TODO: get logged-in username into a variable
+
+#TODO: check whether wine is installed or not and install it if not installed
+
+#TODO: provide multiple download mirrors, and download the visual prolog installer program
+
+#TODO: run the visual prolog installer in wine
+
+#TODO: make sure the installation was successful
+
+#TODO: create a symlink to /home/<username>/.wine/drive_c/VIP52/BIN/WIN/32/Vip.exe in /usr/local/share/visualprolog/
+
+#TODO: copy the desktop file into /usr/local/share/Applications/
+
+#TODO: copy the icon to /usr/local/share/icons
